80db69e18c00a6f2a3625bd46063c6a301958b2d
GregoryDalessandro <gregorydalessandro942@gmail.com>
2018-09-24 22:13:47 -0400

    dynamically render exampleVideoData via modifying templates

--- src/views/videoList.js
+++ src/views/videoList.js
@@ -5,27 +5,27 @@ var VideoListView = Backbone.View.extend({
 
   },
 
-  // events: {
-  //   'change': 'render'
-  // },
-
   render: function() {
     this.$el.children().detach();
     this.$el.html(this.template());
-    console.log('Children',$('.video-list').children());
-    var videoListChildren = $('.video-list').children();
-    // for (var i = 0; i < videoListChildren.length; i++) {
-    //   console.log(typeof videoListChildren[i]);
-    //   $(videoListChildren[i]).replaceWith(this.renderVideo(this.collection[i]));
-    // }
-    this.collection.forEach(this.renderVideo, this);
+    this.$('.video-list').append(
+      this.collection.map(function(video) {
+        return new VideoListEntryView({model: video}).render();
+      }));
     return this;
+    // console.log('Children', $('.video-list').children());
+    // var videoListChildren = $('.video-list').children();
+    // // for (var i = 0; i < videoListChildren.length; i++) {
+    // //   console.log(typeof videoListChildren[i]);
+    // //   $(videoListChildren[i]).replaceWith(this.renderVideo(this.collection[i]));
+    // // }
+    // this.collection.forEach(this.renderVideo, this);
   },
 
-  renderVideo: function(video) {
-    var videoListEntryView = new VideoListEntryView({model: video});
-    this.$el.append(videoListEntryView.render());
-  },
+  // renderVideo: function(video) {
+  //   var videoListEntryView = new VideoListEntryView({model: video});
+  //   this.$el.append(videoListEntryView.render());
+  // },
 
   template: templateURL('src/templates/videoList.html')
 
