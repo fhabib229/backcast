8c16b1823febf27290d63afe86613ab5a4dc91b7
Faris Habib <faris.habib.7@gmail.com>
2018-09-25 12:01:32 -0700

    Hooked videoplayer to videolistview

--- src/views/videoListEntry.js
+++ src/views/videoListEntry.js
@@ -1,15 +1,14 @@
 var VideoListEntryView = Backbone.View.extend({
 
-  initialize: function() {
-    this.on('change', this.render, this);
-  },
+  // initialize: function() {
+  //   this.on('change', this.render, this);
+  // },
 
   events: {
     'click .video-list-entry-title': 'onClickTitle'
   },
 
   onClickTitle: function() {
-    console.log('yay');
     this.model.select();
     new VideoPlayerView({el: $('.player'), model: this.model}).render();
   },
