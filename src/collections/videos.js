e954d68d3d6d6d542693e882af9e614a207cd46c
Faris Habib <faris.habib.7@gmail.com>
2018-09-25 20:59:54 -0700

    Working on implementing Youtube API

--- src/collections/videos.js
+++ src/collections/videos.js
@@ -1,5 +1,20 @@
 var Videos = Backbone.Collection.extend({
+  // url: '/videos',
 
+  search: function(string) {
+    $.ajax({
+      query: string, //FIX ME
+      type: 'GET',
+      contentType: 'application/json',
+      success: function(data) {
+        console.log('Message received');
+        return data;
+      },
+      error: function(data) {
+        console.log('Failed to receive message: ', data);
+      }
+    });
+  },
   model: Video,
 
   parse: function(data) {
