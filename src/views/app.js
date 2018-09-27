b3dcf80885acf8f35e21d439e082100b4e789fe5
Faris Habib <faris.habib.7@gmail.com>
2018-09-26 19:40:48 -0700

    Passed all AppView tests

--- src/views/app.js
+++ src/views/app.js
@@ -5,6 +5,10 @@ var AppView = Backbone.View.extend({
   videos: new Videos(),
 
   initialize: function() {
+    console.log('Videos,',this.videos);
+    this.listenTo(this.videos, 'sync', function() {
+      this.videos.at(0).select();
+    });
     this.videos.search('bananas');
     this.render();
   },
@@ -14,6 +18,7 @@ var AppView = Backbone.View.extend({
     this.$el.html(this.template());
     new VideoPlayerView({el: $('.player'), model: this.videos.at(0), collection: this.videos}).render();
     new VideoListView({el: $('.list'), collection: this.videos}).render();
+    new SearchView({el: $('.search'), collection: this.videos}).render();
     return this;
   },
 
