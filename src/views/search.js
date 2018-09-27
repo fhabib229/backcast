b25a4f496a769bf776ea8641a0441c24f6a8b6bf
Faris Habib <faris.habib.7@gmail.com>
2018-09-26 20:30:02 -0700

    Passed Search tests

--- src/views/search.js
+++ src/views/search.js
@@ -1,5 +1,17 @@
 var SearchView = Backbone.View.extend({
 
+  events: {
+    'click button': 'onSearch',
+    'keyup': 'onSearch'
+  },
+
+  onSearch: function(e) {
+    this.collection.search(this.$('.form-control').val());
+    if (e.which === 13) {
+      this.collection.search(this.$('.form-control').val());
+    }
+  },
+
 
   render: function() {
     this.$el.html(this.template());
